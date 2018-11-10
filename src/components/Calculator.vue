aae55d48e4816f139bbf13630eac7518dbbc4b14
umer khan <umer.kdev@gmail.com>
2018-11-10 19:05:40 +0200

    adding styles to layout

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,6 +1,25 @@
 <template>
     <div class="calculator">
-     <p>I am Calculator</p>
+        <div class="display">123</div>
+            <div class="btn AC">AC</div>
+            <div class="btn">%</div>
+            <div class="btn">/</div>
+            <div class="btn">7</div>
+            <div class="btn">8</div>
+            <div class="btn">9</div>
+            <div class="btn">x</div>
+            <div class="btn">4</div>
+            <div class="btn">5</div>
+            <div class="btn">6</div>
+            <div class="btn">-</div>
+            <div class="btn">1</div>
+            <div class="btn">2</div>
+            <div class="btn">3</div>
+            <div class="btn">+</div>
+            <div class="btn zero">0</div>
+            <div class="btn">.</div>
+            <div class="btn">=</div>
+
     </div>
 </template>
 
@@ -17,5 +36,36 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
+    .calculator {
+        display: grid;
+        font-size: 40px;
+        grid-template-columns: repeat(4, 1fr);
+        grid-auto-rows: minmax(50px, auto);
+    }
+
+    .display {
+        grid-column: 1/5;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        border: 1px solid black;
+    }
+
+    .AC {
+        grid-column: 1/3;
+
+    }
+
+    .zero {
+        grid-column: 1/3;
+    }
+
+    .btn {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border: 1px solid black;
+    }
 
 </style>
